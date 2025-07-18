13082fd8545a8024cedd380a386d585b8dc49cf2
Igor Vieira <igor.p.r.vieira@gmail.com>
2025-07-17 23:32:41 -0300

    changing the formatting to make sure we only formatting if we have the correct extension

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -18,6 +18,14 @@ return {
         markdown = { "prettier" },
       },
       format_on_save = function(bufnr)
+        local filetype = vim.bo[bufnr].filetype
+        
+        -- Only format if we have a formatter configured for this file type
+        local formatters = conform.list_formatters(bufnr)
+        if #formatters == 0 then
+          return nil -- No formatters available, skip formatting
+        end
+        
         return {
           lsp_fallback = true,
           async = false,
@@ -28,6 +36,6 @@ return {
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
-    end, { desc = "Formatar arquivo ou seleção" })
+    end, { desc = "Format file or selection" })
   end,
 }
